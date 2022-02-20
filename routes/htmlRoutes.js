8a0f363fc6f553206b73c3166d3eba6e36368e72
Justin VanHaven <vanhavenjustin@gmail.com>
2022-02-20 17:12:22 -0500

    major push

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
-  // GET * should return the index.html file.
+  // GET * should return index.html file.
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   })
